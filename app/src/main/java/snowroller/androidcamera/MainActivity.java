7d20cbe7b7de72a1c731cdf0ae7bff6fe4421577
Martin Blomberg <martin.blomberg@outlook.com>
2017-11-06 08:40:51 +0100

    Initial Import Camera

---
+++ app/src/main/java/snowroller/androidcamera/MainActivity.java
@@ -0,0 +1,50 @@
+package snowroller.androidcamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+    private Button mButtonCapture;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = findViewById(R.id.imageView);
+        mButtonCapture = findViewById(R.id.button);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void onCaptureButton(View v)
+    {
+        mButtonCapture.setEnabled(false);
+        dispatchTakePictureIntent();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+            mButtonCapture.setEnabled(true);
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+}
